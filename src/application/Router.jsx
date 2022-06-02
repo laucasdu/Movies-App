d3cc09b175619ad2a7aabd08e6f18e59150ce09c
Laura Castillo <lauracasdu@gmail.com>
2022-06-02 11:12:27 +0200

    testing-1

--- src/application/Router.jsx
+++ src/application/Router.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from '../App';
-import Home from "../pages/Home";
 import { MoviePage } from "../pages/MoviePage";
 
 export default function Router () {
@@ -10,7 +9,6 @@ export default function Router () {
     <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/home" element={<Home />} />
         <Route path="/movies/:id" element={<MoviePage />} />
       </Routes>
     </BrowserRouter>
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import { MovieListF } from '../components/list/MovieListF'
 
-
-
 export default function Home () {
 
 
